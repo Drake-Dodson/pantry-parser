e2ad2530dd8ec2caec22ac274f3bde00edf9e0b1
Joshua VanDrie <VanDrieJoshuaD@JohnDeere.com>
2021-09-06 18:40:45 -0500

    finished home screen

--- experiments/Josh_Van_Drie/Experiment1/app/src/main/java/com/example/experiment1/MainActivity.java
+++ experiments/Josh_Van_Drie/Experiment1/app/src/main/java/com/example/experiment1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.experiment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private String adminPassword = "2_do_8_comS_309";
 
     boolean isValid = false;
-    private int counter;
+    private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,27 +42,31 @@ public class MainActivity extends AppCompatActivity {
                 if (inputName.isEmpty() || inputPassword.isEmpty()) {
                     Toast.makeText(MainActivity.this, "Please enter username and password", Toast.LENGTH_SHORT).show();
                 } else {
-                    isValid = validate(inputName,inputPassword);
-                    if(!isValid){
-counter --;
+                    isValid = validateAdmin(inputName, inputPassword);
+                    if (!isValid) {
+                        counter--;
                         Toast.makeText(MainActivity.this, "Incorrect Username or Password", Toast.LENGTH_SHORT).show();
+                        eAttemptsInfo.setText("No. of attempts remaining: " + counter);
 
-                        if(counter == 0){
+                        if (counter == 0) {
                             eLogin.setEnabled(false);
                         }
-                        else{
-                            Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                            // go to new activity
-
-                        }
-
+                    } else {
+                        Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this, HomePage.class);
+                        startActivity(intent);
+                        counter = 5;
+                        eAttemptsInfo.setText("No. of attempts remaining: " + counter);
                     }
+
                 }
             }
+
         });
 
     }
-    private boolean validate (String name, String password){
+
+    private boolean validateAdmin(String name, String password) {
         if (name.equalsIgnoreCase(adminUserName) && password.equals(adminPassword)) {
             return true;
         }

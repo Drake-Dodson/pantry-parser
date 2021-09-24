18db2c6b38db715a4f5f7004130dd5c77b8c9ea3
Drake Dodson <drwdodson@gmail.com>
2021-09-23 19:20:36 -0500

    Changed to requestmapping

--- backend/Pantry_Parser_Backend/src/main/java/com/example/pantryparserbackend/users/UserController.java
+++ backend/Pantry_Parser_Backend/src/main/java/com/example/pantryparserbackend/users/UserController.java
@@ -1,12 +1,6 @@
 package com.example.pantryparserbackend.users;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -16,7 +10,7 @@ public class UserController {
         return "Pantry Parser Super Cool Homepage";
     }
 
-    @GetMapping("/user")
+    @RequestMapping(method = RequestMethod.GET, path = "/user")
     public String getUsername(@RequestParam(value = "username", defaultValue = "World") String message)
     {
         return String.format("Hello, %s! You sent a get request with a parameter!", message);

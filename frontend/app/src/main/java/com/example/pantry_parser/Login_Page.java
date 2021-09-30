e73019c7ef2bcd84f6aab9ea91462a6d72beaf8e
adang <adang@iastate.edu>
2021-09-30 16:27:10 -0500

    added volley to login page

--- frontend/app/src/main/java/com/example/pantry_parser/Login_Page.java
+++ frontend/app/src/main/java/com/example/pantry_parser/Login_Page.java
@@ -1,7 +1,5 @@
 package com.example.pantry_parser;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +7,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.example.pantry_parser.services.RequestListener;
+import com.example.pantry_parser.services.VolleyListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Login_Page extends AppCompatActivity implements View.OnClickListener{
 
@@ -22,7 +29,7 @@ public class Login_Page extends AppCompatActivity implements View.OnClickListene
     private String adminUserName = "Admin";
     private String adminPassword = "2_do_8_comS_309";
 
-    boolean isValid = false;
+    boolean isAdmin = false;
     private int counter = 5;
 
     @Override
@@ -43,53 +50,72 @@ public class Login_Page extends AppCompatActivity implements View.OnClickListene
         eGuest.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        Intent intentSignUp = new Intent(getApplicationContext(), Registration_Page.class);
+        Intent intentLogin = new Intent(Login_Page.this, Home_Page.class);
 
+        switch (view.getId()){
+            case R.id.button_Login:
+                String inputName = eName.getText().toString();
+                String inputPassword = ePassword.getText().toString();
+
+                isAdmin = validateAdmin(inputName, inputPassword);
+                if(isAdmin){
+                    startActivity(intentLogin);
+                }
+                else if (inputName.isEmpty() || inputPassword.isEmpty()) {
+                    Toast.makeText(Login_Page.this, "Please enter username and password", Toast.LENGTH_SHORT).show();
+                } else {
+                    try{
+                        sendLoginInfo(eName.getText().toString(), ePassword.getText().toString());
+                    } catch (JSONException jsonException) {
+                        jsonException.printStackTrace();
+                    }
+                    if (counter == 0) {
+                        eLogin.setEnabled(false);
+                    }
+                }
+                break;
+                case R.id.bt_Guest:
+                    Toast.makeText(Login_Page.this, "logged in as Guest", Toast.LENGTH_SHORT).show();
+                    startActivity(intentLogin);
+                    break;
+                case R.id.text_SignUp:
+                    startActivity(intentSignUp);
+        }
+    }
+
+    private void sendLoginInfo(String eName, String ePassword) throws JSONException {
+        RequestListener loginListener = new RequestListener(){
 
             @Override
-            public void onClick(View view) {
+            public void onSuccess(Object jsonObject) {
+                JSONObject object = (JSONObject)jsonObject;
                 Intent intentLogin = new Intent(Login_Page.this, Home_Page.class);
-
-                switch (view.getId()){
-                    case R.id.button_Login:
-                        String inputName = eName.getText().toString();
-                        String inputPassword = ePassword.getText().toString();
-
-                        if (inputName.isEmpty() || inputPassword.isEmpty()) {
-                            Toast.makeText(Login_Page.this, "Please enter username and password", Toast.LENGTH_SHORT).show();
-                        } else {
-                            isValid = validateAdmin(inputName, inputPassword);
-                            if (!isValid) {
-                                counter--;
-                                Toast.makeText(Login_Page.this, "Incorrect Username or Password", Toast.LENGTH_SHORT).show();
-                                eAttemptsInfo.setText("No. of attempts remaining: " + counter);
-
-                                if (counter == 0) {
-                                    eLogin.setEnabled(false);
-                                }
-                            } else {
-                                Toast.makeText(Login_Page.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                                startActivity(intentLogin);
-                                counter = 5;
-                                eAttemptsInfo.setText("No. of attempts remaining: " + counter);
-                            }
-
-                        }
-                        break;
-                    case R.id.bt_Guest:
-                        Toast.makeText(Login_Page.this, "logged in as Guest", Toast.LENGTH_SHORT).show();
-                        startActivity(intentLogin);
-                        break;
-                    case R.id.text_SignUp:
-                        startActivity(intentLogin);
+                try{
+                    intentLogin.putExtra("message", object.get("message").toString());
+                    startActivity(intentLogin);
+                    counter = 5;
+                    eAttemptsInfo.setText("No. of attempts remaining: " + counter);
                 }
+                catch(JSONException jsonException){
+                    System.out.println("connection issues");
+                }
+            }
+            @Override
+            public void onFailure(String error) {
+                counter--;
+                Toast.makeText(Login_Page.this, "Incorrect Username or Password", Toast.LENGTH_SHORT).show();
+                eAttemptsInfo.setText("No. of attempts remaining: " + counter);
+            }
+        };
 
+        JSONObject data = new JSONObject();
+        data.put("username", eName);
+        data.put("password", ePassword);
 
-
-
-
-
-
-
+        VolleyListener.makeRequest(getApplicationContext(), "077e2f47", loginListener, data, Request.Method.POST);
     }
 
     private boolean validateAdmin(String name, String password) {

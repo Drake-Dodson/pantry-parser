20ddf5e7f1350eefd413c16d125aada17dbc15b0
Paul Brinkmann <pbrink21@iastate.edu>
2021-11-02 17:30:19 -0500

    new tests

--- backend/Pantry_Parser_Backend/src/test/java/com/example/pantryparserbackend/users/UserControllerTest.java
+++ backend/Pantry_Parser_Backend/src/test/java/com/example/pantryparserbackend/users/UserControllerTest.java
@@ -1,5 +1,8 @@
 package com.example.pantryparserbackend.users;
 
+import com.example.pantryparserbackend.Recipes.Recipe;
+import com.example.pantryparserbackend.Recipes.RecipeController;
+import com.example.pantryparserbackend.Recipes.RecipeRepository;
 import com.example.pantryparserbackend.Util.MessageUtil;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -7,14 +10,19 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class UserControllerTest {
     @InjectMocks
     UserController userController;
+
     @Mock
     private UserRepository userRepository;
+    @Mock
+    private RecipeRepository recipeRepository;
 
     // Work In Progress unit tests
     @Test
@@ -64,6 +72,21 @@ class UserControllerTest {
         String expected = MessageUtil.newResponseMessage(false, "password incorrect");
         String actual = userController.login(mockLogin);
         assertEquals(expected, actual);
+    }
+    @Test
+    public void testFavorite_whenNewFavorite_ThenReturnSuccess() {
+        MockitoAnnotations.openMocks(this);
+        User mockUser = new User("password1", "mockitoUserTest@email.com");
+        Recipe mockRecipe = new Recipe("Drake's soup", 4, "Very stinky", "This is a description");
+        int user_id = 1;
+        int recipe_id = 1;
+        when(userRepository.findById(user_id)).thenReturn(mockUser);
+        when(recipeRepository.findById(recipe_id)).thenReturn(mockRecipe);
+
+        String expected = MessageUtil.newResponseMessage(true, "favorited");
+        String actual = userController.favorite(user_id, recipe_id);
+        assertEquals(actual, expected);
+        Mockito.verify(userRepository).save(mockUser);
     }
     @Test
     public void testGetUserByEmail_WhenEmailFound_ThenReturnSuccess() {

43cf3b5015347fb3115727cf670bebd961b4dc39
Paul Brinkmann <pbrink21@iastate.edu>
2021-11-02 15:36:36 -0500

    experimental tests

--- backend/Pantry_Parser_Backend/src/test/java/com/example/pantryparserbackend/users/UserControllerTest.java
+++ backend/Pantry_Parser_Backend/src/test/java/com/example/pantryparserbackend/users/UserControllerTest.java
@@ -1,13 +1,35 @@
 package com.example.pantryparserbackend.users;
 
+import com.example.pantryparserbackend.Util.MessageUtil;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class UserControllerTest {
+    @InjectMocks
+    UserController userController;
+    @Mock
+    private UserRepository userRepository;
 
     // Work In Progress unit tests
+    @Test
+    public void testRegister_NoDuplicate_ThenReturnSuccess() {
+        User mockUser = new User("password", "mockitoUserTest@email.com");
+        when(userRepository.save(mockUser)).thenReturn(mockUser);
+        String expected = MessageUtil.newResponseMessage(true, "User created");
+        String actual = userController.createUser(mockUser);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testRegister_Duplicate_ThenReturnFail() {
+        User mockUser = new User("password", "mockitoUserTest@email.com");
+        when(userRepository.save(mockUser)).thenThrow(new Exception("bad"));
+        String expected = MessageUtil.newResponseMessage(false, "Email already used");
+        String actual = userController.createUser(mockUser);
+        assertEquals(expected, actual);
+    }
     @Test
     public void testGetUserByEmail_WhenEmailFound_ThenReturnSuccess() {
 

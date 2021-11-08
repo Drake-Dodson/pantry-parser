f316acf4c5d2821172cb552a56143a8bab06b59f
Drake Dodson <drwdodson@gmail.com>
2021-11-07 23:44:42 -0600

    I would say I got swagger working but...

--- backend/Pantry_Parser_Backend/src/main/java/com/example/pantryparserbackend/configuration/Swagger2UiConfiguration.java
+++ backend/Pantry_Parser_Backend/src/main/java/com/example/pantryparserbackend/configuration/Swagger2UiConfiguration.java
@@ -12,7 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @Configuration
-@EnableSwagger2
 public class Swagger2UiConfiguration implements WebMvcConfigurer
 {
     @Bean

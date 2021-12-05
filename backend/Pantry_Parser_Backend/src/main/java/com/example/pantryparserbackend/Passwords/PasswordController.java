ff7655ee6513d58279f3e697de90fb995207f7de
Paul Brinkmann <pbrink21@iastate.edu>
2021-12-05 16:03:23 -0600

    added swaggers and javadocs

--- backend/Pantry_Parser_Backend/src/main/java/com/example/pantryparserbackend/Passwords/PasswordController.java
+++ backend/Pantry_Parser_Backend/src/main/java/com/example/pantryparserbackend/Passwords/PasswordController.java
@@ -6,12 +6,15 @@ import com.example.pantryparserbackend.Users.UserRepository;
 import com.example.pantryparserbackend.Services.EmailService;
 import com.example.pantryparserbackend.Utils.MessageUtil;
 import com.example.pantryparserbackend.Services.PasswordService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@Api(value = "Password Controller", description = "Controller for password and OTP functionality")
 public class PasswordController {
 
 	@Autowired
@@ -21,6 +24,12 @@ public class PasswordController {
 	@Autowired
 	private EmailService emailService;
 
+	/**
+	 * Sends an OTP for email verification to the provided user
+	 * @param user_id input user
+	 * @return successful verification or failure message
+	 */
+	@ApiOperation(value = "Sends an OTP for verifying an email address")
 	@GetMapping(path = "/user/{user_id}/verify-email")
 	public String sendVerifyOTP(@PathVariable int user_id) {
 		User user = userRepository.findById(user_id);
@@ -31,6 +40,13 @@ public class PasswordController {
 		return emailService.sendOTPEmail("VerifyEmail", user);
 	}
 
+	/**
+	 * Verifies the email of the provided user
+	 * @param user_id input user
+	 * @param OTP input OTP
+	 * @return successful verification or failure message
+	 */
+	@ApiOperation(value = "Takes in an OTP and determines if it is valid. If so, verifies email")
 	@PostMapping(path = "/user/{user_id}/verify-email")
 	public String verifyEmail(@PathVariable int user_id, @RequestBody String OTP) {
 		User user = userRepository.findById(user_id);
@@ -51,6 +67,7 @@ public class PasswordController {
 	 * @param email email of the user
 	 * @return string message success or failure
 	 */
+	@ApiOperation(value = "Sends an OTP for resetting a user's password")
 	@GetMapping(path = "/user/email/{email}/password-reset")
 	public String sendResetOTP(@PathVariable String email) {
 		User user = userRepository.findByEmail(email);
@@ -67,6 +84,7 @@ public class PasswordController {
 	 * @param request the inputted values in the request
 	 * @return string success or fail
 	 */
+	@ApiOperation(value = "Changes the user's password if they send in a valid OTP")
 	@PostMapping(path = "/user/email/{email}/password-reset")
 	public String resetPassword(@PathVariable String email, @RequestBody PasswordResetRequest request) {
 		User user = userRepository.findByEmail(email);
@@ -81,6 +99,7 @@ public class PasswordController {
 	 * @param user_id id of the user
 	 * @return string success or fail
 	 */
+	@ApiOperation(value = "Sends an OTP for changing a user's password")
 	@GetMapping(path = "/user/{user_id}/password-change")
 	public String sendChangeOTP(@PathVariable int user_id) {
 		User user = userRepository.findById(user_id);
@@ -97,6 +116,7 @@ public class PasswordController {
 	 * @param request otp and new password
 	 * @return string success or fail
 	 */
+	@ApiOperation(value = "Changes a user's password if they send in a valid OTP")
 	@PostMapping(path = "/user/{user_id}/password-change")
 	public String changePassword(@PathVariable int user_id, @RequestBody PasswordResetRequest request) {
 		User user = userRepository.findById(user_id);

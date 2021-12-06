ae6f1541d2378045c239a632be5ff68923e45263
adang <adang@iastate.edu>
2021-12-06 11:51:23 -0600

    fixed merge conflicts

--- frontend/app/src/main/java/com/example/pantry_parser/Utilities/URLs.java
+++ frontend/app/src/main/java/com/example/pantry_parser/Utilities/URLs.java
@@ -4,6 +4,7 @@ public class URLs {
     private static final String DOMAIN = "http://coms-309-032.cs.iastate.edu:8080";
     public static final String URL_REGISTER = DOMAIN + "/user/";
     public static final String URL_LOGIN = DOMAIN + "/login/";
+    public static final String URL_USER = DOMAIN + "/user/";
 
     public static String paginatedQueryURLComposer(String base, String query, int pageNo, int perPage) {
         return base + "?query=" + query + "&pageNo=" + pageNo + "&perPage=" + perPage;

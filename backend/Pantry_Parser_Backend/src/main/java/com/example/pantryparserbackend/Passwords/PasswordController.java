043235ea581047ba6f31c0682c2634dde3d1240e
Paul Brinkmann <pbrink21@iastate.edu>
2021-12-05 23:41:02 -0600

    fixed big dum mistake that i made

--- backend/Pantry_Parser_Backend/src/main/java/com/example/pantryparserbackend/Passwords/PasswordController.java
+++ backend/Pantry_Parser_Backend/src/main/java/com/example/pantryparserbackend/Passwords/PasswordController.java
@@ -10,12 +10,10 @@ import com.example.pantryparserbackend.Services.PasswordService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Api(value = "Password Controller", description = "Controller for password and OTP functionality")
+@RestController
 public class PasswordController {
 
 	@Autowired

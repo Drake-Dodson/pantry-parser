fdb6c3b4b4665c97f70e1160a7a20bd85401083a
adang <adang@iastate.edu>
2021-12-06 11:37:09 -0600

    request buttons now have 3 second delay before being able to click again

--- frontend/app/src/main/java/com/example/pantry_parser/Pages/Settings/ChangePassword_Page.java
+++ frontend/app/src/main/java/com/example/pantry_parser/Pages/Settings/ChangePassword_Page.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -72,6 +73,13 @@ public class ChangePassword_Page extends AppCompatActivity {
                 if(user_id != "") {
                     sendOTP();
                 }
+                bt_sendOTP.setEnabled(false);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        bt_sendOTP.setEnabled(true);
+                    }
+                }, 3000);
             }
         });
 
@@ -90,6 +98,13 @@ public class ChangePassword_Page extends AppCompatActivity {
                 } else {
                     Toast.makeText(getApplicationContext(), "Please make sure all fields are filled", Toast.LENGTH_SHORT).show();
                 }
+                bt_confirm.setEnabled(false);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        bt_confirm.setEnabled(true);
+                    }
+                }, 3000);
             }
         });
     }
--- frontend/app/src/main/java/com/example/pantry_parser/Pages/Settings/ChangeUsername_Page.java
+++ frontend/app/src/main/java/com/example/pantry_parser/Pages/Settings/ChangeUsername_Page.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -71,7 +72,13 @@ public class ChangeUsername_Page extends AppCompatActivity {
                 } else {
                     Toast.makeText(ChangeUsername_Page.this, "Please make sure you have entered a new username and your password.", Toast.LENGTH_SHORT).show();
                 }
-
+                bt_submit.setEnabled(false);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        bt_submit.setEnabled(true);
+                    }
+                }, 3000);
             }
         });
     }

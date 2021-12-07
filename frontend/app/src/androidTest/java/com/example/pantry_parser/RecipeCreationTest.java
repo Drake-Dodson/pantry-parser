85a6bee76b253d772a16fd26efe2a0fc1d00bdca
adang <adang@iastate.edu>
2021-12-07 12:09:09 -0600

    fixed issue with clicking signup on login page

---
+++ frontend/app/src/androidTest/java/com/example/pantry_parser/RecipeCreationTest.java
@@ -0,0 +1,24 @@
+package com.example.pantry_parser;
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class RecipeCreationTest {
+
+@Test
+public void whenCreateRecipeObject_thenDoNothing() {
+    String name = "name";
+    String summary = "This is a test summary";
+    String description = "This is a test description";
+    String nutritionFacts = "This is a test nutrition fact";
+    int num_servings = 4;
+    int prep_time = 10;
+    int cook_time = 20;
+
+}
+
+
+}
